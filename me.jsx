9c8033b7c31b056e3c9912f3c3094d685cd8d896
puttachai <puttachai.2546@gmail.com>
2024-11-29 10:05:31 +0700

    Update Me component

--- me.jsx
+++ me.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function Me() {
-  return <p>This is the Me component.</p>;
+  return (
+    <div>
+        <p>This is the Me component.</p>;
+        <p>This is the Me React.</p>;
+    </div>
+  )
 }
 
 export default Me;
